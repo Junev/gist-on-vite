f36d688edce9a663c1dbade474e87e1acee16b90
Junev <lijun58518@126.com>
2022-11-03 14:07:04 +0800

    add unplugin-auto-import unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import fs from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
@@ -32,8 +35,9 @@ function getRollupInput () {
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: getRollupInput()
-    }
+      input: { index: resolve(__dirname, "index.html")}
+    },
+    sourcemap: true
   },
   resolve: {
     alias: {
@@ -49,19 +53,48 @@ export default defineConfig({
   // },
   plugins: [
     vue(),
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue', '@vueuse/core'],
+      dirs: [path.resolve(pathSrc, 'composables')],
+
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+      vueTemplate: true,
+      dts: path.resolve(pathSrc, 'typings', 'auto-imports.d.ts'),
+    }),
     Components({
       // allow auto load markdown components under `./src/components/`
       extensions: ['vue', 'md'],
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
         ElementPlusResolver({
           importStyle: 'sass',
         }),
       ],
       dts: 'src/components.d.ts',
     }),
-
+    Icons({
+      autoInstall: true,
+    }),
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
     Unocss({
